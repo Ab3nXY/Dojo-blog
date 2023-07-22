2e56764d6c710c6c12f5c8a026ac2f83c3aa914d
Ab3nXY <127937158+Ab3nXY@users.noreply.github.com>
2023-07-22 04:44:41 +0300

    Upto lesson 18

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,29 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+
+
 const Home = () => {
+   const [blogs, setBlogs] = useState(null);
+   const [isPending, setIsPending] = useState(true);
+
+   useEffect(() => {
+      setTimeout(() => {
+         fetch('http://localhost:8000/blogs')
+      .then(res => {
+         return res.json();
+      })
+
+      .then(data => {
+         setBlogs(data);
+         setIsPending(false);
+      });
+   },1000)
+   },[]);
+
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
+         { isPending && <div>Loading...</div> }
+         { blogs && <BlogList blogs={blogs} title="All Blogs" /> }
         </div>
      );
 }
